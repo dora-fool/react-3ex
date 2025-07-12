3237641a3f641385d4bc26555c292d4bbb691d26
dora-fool <drama_guen@vk.com>
2025-07-12 14:08:58 +0500

    Исправлено Counter

--- my-app1/src/Counter.jsx
+++ my-app1/src/Counter.jsx
@@ -4,13 +4,11 @@ function Counter() {
   const [count, setCount] = useState(0)
 
   const increment = () => {
-    setCount (count + 1)
+    setCount (prevCount => prevCount + 1)
   }
 
   const decrement = () => {
-    if (count > 0) {
-      setCount (count - 1)
-    }
+    setCount(prevCount => (prevCount > 0 ? prevCount - 1 : prevCount))
   }
 
   return (
